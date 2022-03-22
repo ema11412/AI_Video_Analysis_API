55d34909fe0fd2c15a0851d026a8eded6d1b564c
Emanuel Esquivel <ema11412@hotmail.com>
2022-03-22 14:24:41 -0600

    [UPDATE][AlphaPredict]
    Se empaquetó el predictor alpha

--- application/components/alphaPredict/alphaPredict.py
+++ application/components/alphaPredict/alphaPredict.py
@@ -5,64 +5,98 @@ from PIL import Image
 import imagehash
 import pandas as pd
 import numpy as np
+from scipy.spatial.distance import hamming
+import json
 
 df = pd.read_csv("../../assets/data/values1.csv")
 
-prueba = 3
+#prueba = 0
 
-t1 = f'../../assets/test/t{prueba}.jpg'
-ultVal = int(df["id"].iloc[-1][1:])
+def alphaAnalysis(prueba):
+    t1 = f'../../assets/test/t{prueba}.jpg'
+    ultVal = int(df["id"].iloc[-1][1:])
 
-# test image
-image = cv2.imread(t1)
-gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-histogram = cv2.calcHist([gray_image], [0], None, [256], [0, 256])
-
-cs = []
-
-c1, c2 = 0, 0
-
-for i in range(ultVal+1):
-    # data
-    image = cv2.imread(f'../../assets/img/d{i}.jpg')
-    gray_image1 = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    histogram1 = cv2.calcHist([gray_image1], [0], None, [256], [0, 256])
-    i = 0
-    while i<len(histogram) and i<len(histogram1):
-        c1+=(histogram[i]-histogram1[i])**2
-        i+= 1
-    c1 = c1**(1 / 2)
-    cs += [c1[0]]
-
-ind = cs.index(min(cs))
-
-print(f"La imagen es comparable con {ind}")
-
-fig, (ax1, ax2) = plt.subplots(1, 2)
+    # test image
+    image = cv2.imread(t1)
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    histogram = cv2.calcHist([gray_image], [0], None, [256], [0, 256])
 
 
-A = mpimg.imread(f'../../assets/img/d{ind}.jpg')
-B = mpimg.imread(t1)
+    cs = []
 
-ax1.imshow(B)
-ax1.set_title('Imagen original')
-ax2.imshow(A)
-ax2.set_title("Imagen comparable")
+    c1, c2 = 0, 0
 
-plt.show()
+    for i in range(ultVal+1):
+        # data
+        image = cv2.imread(f'../../assets/img/d{i}.jpg')
+        gray_image1 = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        histogram1 = cv2.calcHist([gray_image1], [0], None, [256], [0, 256])
+        i = 0
+        while i<len(histogram) and i<len(histogram1):
+            c1+=(histogram[i]-histogram1[i])**2
+            i+= 1
+        c1 = c1**(1 / 2)
+        cs += [c1[0]]
+
+    ind = cs.index(min(cs))
+
+    print(f"La imagen es comparable con {ind}")
+
+    fig, (ax1, ax2) = plt.subplots(1, 2)
 
 
-# hashing
-hash0 = imagehash.average_hash(Image.open(t1)) 
-hash1 = imagehash.average_hash(Image.open(f'../../assets/img/d{ind}.jpg')) 
-compHash = hash0 - hash1
+    A = mpimg.imread(f'../../assets/img/d{ind}.jpg')
+    B = mpimg.imread(t1)
+
+    ax1.imshow(B)
+    ax1.set_title('Imagen original')
+    ax2.imshow(A)
+    ax2.set_title("Imagen comparable")
+
+    #plt.show()
+
+    # hashing
+    ahash1 = imagehash.average_hash(Image.open(t1)) 
+
+    ahash0 = imagehash.average_hash(Image.open(f'../../assets/img/d{ind}.jpg')) 
+
+    # ahash = df[df['id']==f'd{ind}']['ahash']
+    # ahash0 = ahash.tolist()[0]
+    # ahash1 = str(imagehash.average_hash(Image.open(t1)) )
+
+    # print(type(ahash0))
+    # print(type(ahash1))
+
+    # print(ahash0)
+    # print(ahash1)
+
+    # print(hamming(ahash1,ahash0))
+
+    compHash = ahash1 - ahash0
+
+    # # calculo de alpha
+    # valor de Umbral
+    umbral = 20
+    # Error de aproximación
+    errorApr = abs((umbral-compHash)/umbral)/10
+
+    # valor de alpha referente a la imagen encontrada
+    alpha0 = df[df['id']==f'd{ind}']['a']
+
+    val = round(errorApr*alpha0+alpha0)
+    alpha = int(val.tolist()[0])
+
+    # valor de lambda referente a la imagen encontrada
+    lambda0 = df[df['id']==f'd{ind}']['lc']
+
+    val = round(lambda0*errorApr+lambda0)
+    lambdaa = int(val.tolist()[0])
+
+    response = {'alpha': alpha, 'lambda' : lambdaa}
+    #print(f'El valor de alpha es {alpha}, y lambda {lambdaa}')
+
+    return response
 
 
-# calculo de alpha
-alpha0 = df[df['id']==f'd{ind}']['a']
-
-val = round((compHash**2/100)+alpha0)
-alpha = val.tolist()[0]
-
-print(alpha)
-
+prueba = 0
+print(alphaAnalysis(prueba))
